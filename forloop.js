43c38d356119f4c69f75fbcb6513f81e9b49c4ab
SANTOSH HUMNEKAR <santosh.humnekar@gmail.com>
2017-02-24 13:40:39 +0530

    Add files via upload

---
+++ forloop.js
@@ -0,0 +1,16 @@
+var sum = 4;
+            for (var i = 1; i <= 31; i++) {
+               sum = sum + i;
+            };
+
+var text = 4;
+var i = 1;
+while (i < 32) 
+{
+    text = text + i; i++;
+}
+
+var add=sum+text;
+            console.log("For_Sum = " + sum);    // => for loop Sum = 500
+            console.log("While_Sum = " + text); // => While loop Some = 500
+			console.log("For_and_While_Loop_Sum = " + add); // => both loop addition =1000
